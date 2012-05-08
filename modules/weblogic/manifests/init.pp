f030a3c5f76c745837e2fc540ec34893a3a924f8
Wian Vos <wvos@xebia.com>
2012-05-08 16:05:20 +0200

    added prereq to init.pp

--- modules/weblogic/manifests/init.pp
+++ modules/weblogic/manifests/init.pp
@@ -1,5 +1,8 @@
 #testing 123
 class weblogic {
 	include weblogic::params
-	include weblogic::install
+	include weblogic::prereq
+	class{"weblogic::install":
+		require => Class["weblogic::prereq"]
+	}
 }
