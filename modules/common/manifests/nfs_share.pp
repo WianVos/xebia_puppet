507cc84505e28011e8d39cbeaad6fa6e3778740a
Wian Vos <wvos@xebia.com>
2012-05-08 15:46:02 +0200

    added nfs

---
+++ modules/common/manifests/nfs_share.pp
@@ -0,0 +1,25 @@
+define common::nfs_share(
+	$path = "$name",
+	$hosts = "",
+	$options = "async,"
+){
+	
+	package{"${name} nfs_server":
+		name => $::operatingsystem ?{
+				ubuntu => "unfs3",
+				default => "unfs3"},
+		ensure => present,
+		}
+	
+	case $options {
+		"": 		{ $options_string = ""	}
+		default: 	{ $options_string = "-o ${options}"}
+		} 
+	
+	
+	exec{"export ${hosts}:${path}":
+		command => "/usr/sbin/exportfs ${options_string} ${hosts}:${path} && touch /var/tmp/share_${path}",
+		creates => "/var/tmp/share_${path}"
+	}	
+
+}
