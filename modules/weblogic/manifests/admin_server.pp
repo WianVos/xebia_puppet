4d8fa0033f14bac34a1814fa363b0ef86ee5cfdb
Wian Vos <wvos@xebia.com>
2012-05-08 20:12:36 +0200

    added exec

--- modules/weblogic/manifests/admin_server.pp
+++ modules/weblogic/manifests/admin_server.pp
@@ -16,24 +16,31 @@ class weblogic::admin_server(
 		ensure => directory,
 		}
 	file {"${weblogic::params::infradir}/weblogic/domain_utility.py":
-		source => "puppet:///modules/weblogic/admin_server/domain_utility.py"
+		source => "puppet:///modules/weblogic/admin_server/domain_utility.py",
+		require => File["${weblogic::params::infradir}/weblogic"]
 		}
 	file {"${weblogic::params::infradir}/weblogic/schemes.py":
-		source => "puppet:///modules/weblogic/admin_server/schemes.py"
+		source => "puppet:///modules/weblogic/admin_server/schemes.py",
+		require => File["${weblogic::params::infradir}/weblogic"]
 		}
 	file {"${weblogic::params::infradir}/weblogic/weblogic.py":
-		source => "puppet:///modules/weblogic/admin_server/weblogic.py"
+		source => "puppet:///modules/weblogic/admin_server/weblogic.py",
+		require => File["${weblogic::params::infradir}/weblogic"]
 		}
 	file {"${weblogic::params::infradir}/weblogic/${domain_name}.py":
 		content => template('weblogic/admin_server/environment_template.py.erb'),
+		require => File["${weblogic::params::infradir}/weblogic"]
 		}
 	file {"${weblogic::params::infradir}/weblogic/weblogic_template.py":
 		content => template('weblogic/admin_server/weblogic_template.py.erb'),
+		require => File["${weblogic::params::infradir}/weblogic"]
 		}
 	
 	exec{"${domain_name} domain creation":
 		command => "${weblogic::params::installpath}/wlserver_10.3/common/bin/wlst.sh -i weblogic.py -p ${domain_name}",
 		cwd => "${weblogic::params::infradir}/weblogic",
-		
+		creates => "${weblogic::params::application_base_dir}/${domain_name}",
+		require => File["${weblogic::params::infradir}/weblogic/domain_utility.py","${weblogic::params::infradir}/weblogic/schemes.py","${weblogic::params::infradir}/weblogic/weblogic.py","${weblogic::params::infradir}/weblogic/${domain_name}.py","${weblogic::params::infradir}/weblogic/weblogic_template.py"  ]
+		}
 	
 }
