c1991a185371d658e02fc500c452b11e490c897e
Wian Vos <wvos@xebia.com>
2012-06-10 08:06:12 +0200

    debugging

--- modules/deployit_cli/manifests/features/execute.pp
+++ modules/deployit_cli/manifests/features/execute.pp
@@ -29,14 +29,19 @@ define deployit_cli::features::execute(
 	if ! defined(Class["deployit_cli"]){
 		class{"deployit_cli":} 
 	}
-
-	if ("${username}" == "" ) or ("${password}" == "" ) or ("${host}" == "" ) or ("${port}" == "") {
-		notice "unable to run deployit command"
-	}
-	else {
-		exec { "execute ${source} with params ${params}":
+	exec { "execute ${source} with params ${params}":
 			cwd => "${homedir}",
 			command => "${homedir}/bin/cli.sh -host ${host} -port ${port} -username ${username} -password ${password} -f ${source} -- ${params}",
+			require => Class["deployit_cli"]
 			}
-	}
+
+#	if ("${username}" == "" ) or ("${password}" == "" ) or ("${host}" == "" ) or ("${port}" == "") {
+#		notice "unable to run deployit command"
+#	}
+#	else {
+#		exec { "execute ${source} with params ${params}":
+#			cwd => "${homedir}",
+#			command => "${homedir}/bin/cli.sh -host ${host} -port ${port} -username ${username} -password ${password} -f ${source} -- ${params}",
+#			}
+#	}
 }
