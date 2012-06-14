4e72d36fcb52fec3e5336d90c886973e87673084
Wian Vos <wvos@xebia.com>
2012-06-14 23:26:17 +0200

    refactor

--- modules/xebia_common/manifests/archive/targz.pp
+++ modules/xebia_common/manifests/archive/targz.pp
@@ -23,11 +23,13 @@ define xebia_common::archive::targz (
   require xebia_common::packages
  
   
-  file {"${name}_${target}":
-     path	=> "${target}",	
-     ensure 	=> directory,
-     before	=> Exec["$name download and unpack"],
-     }
+  if !defined(File["${target}"]){
+  	file {"${name}_${target}":
+    	path	=> "${target}",	
+     	ensure 	=> directory,
+     	before	=> Exec["$name download and unpack"],
+     }  
+    }
 
   exec {"$name download and unpack":
     command 	=> "/usr/bin/curl ${source_url} | tar -xzf - -C ${target} && chown -R ${owner}:${group} ${target} && chmod -R ${mode} ${target} && touch ${regdir}/${name}",
