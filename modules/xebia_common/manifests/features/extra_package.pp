707b007f58d205406fc77f55452f2e35a5a0e600
Wian Vos <wvos@xebia.com>
2012-06-23 23:11:36 +0200

    simplified xtra-packages

--- modules/xebia_common/manifests/features/extra_package.pp
+++ modules/xebia_common/manifests/features/extra_package.pp
@@ -1,10 +1,9 @@
 define xebia_common::features::extra_package (
-		$package_name   = $name,
 		$ensure  		= "latest"
 ){
 
-if !defined(Package["${package_name}"]){
-		package{"${package_name}":
+if !defined(Package["${name}"]){
+		package{"${name}":
 			ensure 	=> "${ensure}"
 			}
 	}
