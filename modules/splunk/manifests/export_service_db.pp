8ade37ae28a1643b0a7389e713350a28b8542604
root <root@ip-10-234-89-51.eu-west-1.compute.internal>
2012-06-27 12:37:07 +0000

    added splunk

---
+++ modules/splunk/manifests/export_service_db.pp
@@ -0,0 +1,58 @@
+define postgresql::export_service_db(
+	$timestamp	=	inline_template("<%= Time.now.strftime('%Y-%m-%dT%H:%M:%S%z') %>"),
+	$maxage 	= 	"28800",
+	$service 	= 	"xx",
+	$role		=	"xx",
+	$customer	=	"xx",
+	$application 	=  	"xx",
+	$universe	=	"xx",
+	$tag		=	"S${service}R${role}U${universe}C${customer}A${application}",
+	$hostname	=   "${::hostname}",
+	$config_dir	=	params_lookup('configdir'),
+	$use_concat =   true,
+	$concat_target = "",
+	$concat_options = "",
+	$concat_seperator = " = ",
+	$concat_owner	=	"root",
+	$concat_group	=	"root",
+	$config_options	= 	"",
+	$use_config	= 	false
+	
+){
+	
+	# check age of facts ..
+	$age = inline_template("<%= require 'time'; Time.now - Time.parse(timestamp) %>")
+	
+	if $age < $maxage {
+		if $use_concat {
+			if !defined(Concat["${concat_target}"]){
+				include concat::setup
+				concat {
+					"${concat_target}" :
+					owner => "${concat_owner}",
+					group => "${concat_group}",
+					mode => "0770",				
+				}	
+			}
+			concat::fragment {
+				"${concat_target} ${customer}_${application}_${::hostname}" :
+					target => "${concat_target}",
+					content =>
+					inline_template("<% concat_options.sort_by {|key, value| key}.each do |key, value| %><%= key %><%= concat_seperator %><%= value %> \n<% end %>"),
+					order => 01,
+			}
+		}
+		if $use_config {
+		$config_file	=	"${config_dir}/${service}-${role}.txt"
+		file {
+			"${customer}_${application}_${::hostname}" :
+				path => "${config_file}",
+				require => File["${config_dir}"],
+				tag => $tag,
+				content =>
+				inline_template("<% options.sort_by {|key, value| key}.each do |key, value| %><%= key %>='<%= value %>' \n<% end %>")
+			}
+		
+		}
+	}
+}
---
+++ modules/splunk/manifests/params.pp
@@ -0,0 +1,30 @@
+class splunk::params{
+	
+	
+	$infra_dir				= "/etc/xebia_puppet"
+	$marker_dir 			= "${infra_dir}/marker"
+	$script_dir 			= "${infra_dir}/script"
+	$config_dir 			= "${infra_dir}/config"
+	$tmpdir						= 	"/var/tmp/splunk"
+	$basedir					=	'/opt'
+	$homedir					=	"${basedir}/splunk"
+	$datadir					=	'/data/'
+	$absent 					=	false
+	$ensure						= 	false
+	$disabled 					= 	false
+	$import_facts				= 	true
+	$import_config				= 	true
+	$export_facts				= 	true
+	$export_config				= 	true
+	$universe					=   'default'
+	$customer					=	'default'
+	$application				=	'default'
+	$install					=	'puppetfiles'
+	$install_owner				=	'splunk'
+	$install_group				=	'splunk'
+	$packages					=	['unzip']
+	
+	# splunk specific settings
+	$puppetfiles_tarfile				= "splunk-4.3-115073-Linux-x86_64.tgz"
+	$puppetfiles_source			= "puppet:///modules/splunk/install_tar/${puppetfiles_tarfile}"
+}
