3ab449ee0bc7bf7f0f794aaf94b392afd97264b8
root <root@ip-10-234-89-51.eu-west-1.compute.internal>
2012-07-10 19:11:55 +0000

    refactoring postgresql

--- bin/xebia_create_universe.sh
+++ bin/xebia_create_universe.sh
@@ -1,44 +1,12 @@
 #! /bin/bash
-debug="set -x"
-$debug
+/opt/xebia_puppet/bin/xebia_bootstrap.sh -n deployit_host -u xebia & 
+sleep 60
+/opt/xebia_puppet/bin/xebia_bootstrap.sh -n postgresql_host -u xebia -c lg -R standalone &
+sleep 60 
+/opt/xebia_puppet/bin/xebia_bootstrap.sh -n jetty_host -u xebia -c lg -a test -R single &
+sleep 60
+/opt/xebia_puppet/bin/xebia_bootstrap.sh -n jetty_host -u xebia -c lg -a ontw -R single 
+sleep 30 
+/opt/xebia_puppet/bin/xebia_bootstrap.sh -n jetty_host -u xebia -c lg -a acpt -R single 
 
-#initialize variables
-universe="xx"
-customer="xx"
-application="xx"
-tmpfile=`tempfile`
-size='xx'
-appnames='xx'
-
-
-# parse commandline options
-while getopts u:c:A:s o
-do case "$o" in
-	u)universe=$OPTARG;;
-	c)customer=$OPTARG;;
-	A)appnames=$OPTARG;;
-	s)size=$OPTARG;;
-	?)echo "			"
-		echo "	-u <universe name> " 
-		echo "	-c <customer> "
-		echo "	-a <application> "
-		echo "  -A <app-names> comma separate list" 
-		echo "  -s default size application containers" 
-		exit 0 
-esac
-done
-
-		
-	
-#check and correct input
-if [ $appnames == "xx" ] ; then appnames="default" ; fi
-if [ $universe == "xx" ] ; then universe="default" ; fi
-if [ $customer == "xx" ] ; then customer="xebiaPuppet" ; fi
-if [ $size == "xx" ] ; then size="s" ; fi
-
-#install services
-
-
-
-
-#install containers
+su - peadmin -c 'mco puppetd runonce' 
