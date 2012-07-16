3dcb23dfcd60b503669a2f23fa2584689fc9a1a8
Wian Vos <wvos@xebia.com>
2012-07-17 00:31:18 +0200

    time for bed

--- modules/deployit/manifests/types/jetty_ssh.pp
+++ modules/deployit/manifests/types/jetty_ssh.pp
@@ -7,72 +7,76 @@ define deployit::types::jetty_ssh ($remotehost,
 	$application	= "default",
 	$remotehost	= "",
 	$appstage			= "test"
-	){
-	
-	if ! defined(Deployit::Features::Ci["${customer} dir"]){
-		deployit::features::ci {
-							"${customer} dir" :
-								ciId => "Environments/${customer}",
-								ciType => 'core.Directory',
-								ciValues => {name => "${customer}"},
-								ensure => present
-						}
-	}
-	
-	if ! defined(Deployit::Features::Ci["${customer}/${application} dir"]){
-		deployit::features::ci {
-							"${customer}/${application} dir" :
-								ciId => "Environments/${customer}/${application}",
-								ciType => 'core.Directory',
-								ciValues => {name => "${application}"},
-								ensure => present,
-								require => Deployit::Features::Ci["${customer} dir"]
-						}		
+	) {
 		
-	}
-	
-	if ! defined(Deployit::Features::Ci["${customer}/${application}/${appstage} dir"]){
-		deployit::features::ci{ "${customer}/${application}/${appstage} dir":
- 				 ciId => "${customer}/${application}/${appstage}",
-  				 ciType => 'core.Directory',
-  				 ciValues => { name => "${appstage}"},
-  				 ensure => present,
-  				 require => Deployit::Features::Ci["${customer}/${application} dir"]
+	if !defined(Deployit::Features::Ci["${customer} dir"]) {
+		deployit::features::ci {
+			"${customer} dir" :
+				ciId => "Environments/${customer}",
+				ciType => 'core.Directory',
+				ciValues => {name => "${customer}"},
+				ensure => present
 		}
 	}
-	
-	if ! defined(Deployit::Features::Ci["${customer}/${application}/${appstage}/${application}-${appstage} env"]){
-		deployit::features::ci{ "${customer}/${application}/${appstage}/${application}-${appstage} env":
- 				 ciId => "${customer}/${application}/${appstage}/${application}-${appstage}",
-  				 ciType => 'udm.Environment',
-  				 ciValues => { name => "${application}-${appstage}"  },
-  				 ensure => present,
-  				 require => Deployit::Features::Ci["${customer}/${application} dir","${customer}/${application}/${appstage} dir","${customer} dir"]
+	if !defined(Deployit::Features::Ci["${customer}/${application} dir"]) {
+		deployit::features::ci {
+			"${customer}/${application} dir" :
+				ciId => "Environments/${customer}/${application}",
+				ciType => 'core.Directory',
+				ciValues => {name => "${application}"},
+				ensure => present,
+				require => Deployit::Features::Ci["${customer} dir"]
 		}
 	}
-	
-	if ! defined(Deployit::Features::Ci["${remotehost} ssh-host"]){
-	deployit::features::ci{ "${remotehost} ssh-host":
- 				 ciId => "Infrastructure/${remotehost}",
-  				 ciType => 'overthere.SshHost',
-  				 ciValues => { os => UNIX, connectionType => SUDO, username => 'deployit', password => 'deployit',
-                 		sudoUsername => 'root', address => "${remotefqdn}", privateKeyFile => "/opt/deployit/keys/jetty_id_rsa" },
-                 		ciEnvironments => "${customer}/${application}/${appstage}/${application}-${appstage}",
-  				 ensure => present,
-  				 require => Deployit::Features::Ci["${customer}/${application}/${appstage}/${application}-${appstage} env"]
-  				 
+	if !defined(Deployit::Features::Ci["${customer}/${application}/${appstage} dir"])
+	{
+		deployit::features::ci {
+			"${customer}/${application}/${appstage} dir" :
+				ciId => "Environments/${customer}/${application}/${appstage}",
+				ciType => 'core.Directory',
+				ciValues => {name => "${appstage}"},
+				ensure => present,
+				require => Deployit::Features::Ci["${customer}/${application} dir"]
 		}
 	}
-	
+	if !defined(Deployit::Features::Ci["${customer}/${application}/${appstage}/${application}-${appstage} env"])
+	{
+		deployit::features::ci {
+			"${customer}/${application}/${appstage}/${application}-${appstage} env" :
+				ciId =>
+				"Environments/${customer}/${application}/${appstage}/${application}-${appstage}",
+				ciType => 'udm.Environment',
+				ciValues => {name => "${application}-${appstage}"},
+				ensure => present,
+				require => Deployit::Features::Ci["${customer}/${application} dir",
+				"${customer}/${application}/${appstage} dir", "${customer} dir"]
+		}
+	}
+	if !defined(Deployit::Features::Ci["${remotehost} ssh-host"]) {
+		deployit::features::ci {
+			"${remotehost} ssh-host" :
+				ciId => "Infrastructure/${remotehost}",
+				ciType => 'overthere.SshHost',
+				ciValues => {os => UNIX, connectionType => SUDO, username => 'deployit',
+				password => 'deployit', sudoUsername => 'root', address => "${remotefqdn}",
+				privateKeyFile => "/opt/deployit/keys/jetty_id_rsa"},
+				ciEnvironments =>
+				"${customer}/${application}/${appstage}/${application}-${appstage}",
+				ensure => present,
+				require =>
+				Deployit::Features::Ci["${customer}/${application}/${appstage}/${application}-${appstage} env"]
+		}
+	}
 	deployit::features::ci {
 		"jetty_server_${remotehost}_${instance_name} " :
 			ciId => "Infrastructure/${remotehost}/${instance_name}",
 			ciType => 'jetty.Server',
 			ciValues => {home => "$homedir", startScript => "${homedir}/start.sh",
 			stopScript => "${homedir}/stop.sh"},
-			ciEnvironments => "${customer}/${application}/${appstage}/${application}-${appstage}",
-			require =>
-			Deployit::Features::Ci["${remotehost} ssh-host","${customer}/${application}/${appstage}/${application}-${appstage} env"],
+			ciEnvironments =>
+			"${customer}/${application}/${appstage}/${application}-${appstage}",
+			require => Deployit::Features::Ci["${remotehost} ssh-host",
+			"${customer}/${application}/${appstage}/${application}-${appstage} env"],
 			ensure => present,
 	}
-}
+	}
--- modules/deployit/manifests/types/postgresql_ssh.pp
+++ modules/deployit/manifests/types/postgresql_ssh.pp
@@ -10,61 +10,66 @@ define deployit::types::postgresql_ssh (
 	$remotehost		= "",
 	$appstage		= undef
 	) {
-	if ! defined(Deployit::Features::Ci["${customer} dir"]){
-		deployit::features::ci {
-							"${customer} dir" :
-								ciId => "Environments/${customer}",
-								ciType => 'core.Directory',
-								ciValues => {name => "${customer}"},
-								ensure => present
-						}
-	}
-	
-	if ! defined(Deployit::Features::Ci["${customer}/${application} dir"]){
-		deployit::features::ci {
-							"${customer}/${application} dir" :
-								ciId => "Environments/${customer}/${application}",
-								ciType => 'core.Directory',
-								ciValues => {name => "${application}"},
-								ensure => present,
-								require => Deployit::Features::Ci["${customer} dir"]
-						}		
-		
-	}
-	
-	if ! defined(Deployit::Features::Ci["${customer}/${application}/${appstage} dir"]){
-		deployit::features::ci{ "${customer}/${application}/${appstage} dir":
- 				 ciId => "${customer}/${application}/${appstage}",
-  				 ciType => 'core.Directory',
-  				 ciValues => { name => "${appstage}"},
-  				 ensure => present,
-  				 require => Deployit::Features::Ci["${customer}/${application} dir"]
+		if !defined(Deployit::Features::Ci["${customer} dir"]) {
+			deployit::features::ci {
+				"${customer} dir" :
+					ciId => "Environments/${customer}",
+					ciType => 'core.Directory',
+					ciValues => {name => "${customer}"},
+					ensure => present
+			}
 		}
-	}
-	
-	if ! defined(Deployit::Features::Ci["${customer}/${application}/${appstage}/${application}-${appstage} env"]){
-		deployit::features::ci{ "${customer}/${application}/${appstage}/${application}-${appstage} env":
- 				 ciId => "${customer}/${application}/${appstage}/${application}-${appstage}",
-  				 ciType => 'udm.Environment',
-  				 ciValues => { name => "${application}-${appstage}"  },
-  				 ensure => present,
-  				 require => Deployit::Features::Ci["${customer}/${application} dir","${customer}/${application}/${appstage} dir","${customer} dir"]
+		if !defined(Deployit::Features::Ci["${customer}/${application} dir"]) {
+			deployit::features::ci {
+				"${customer}/${application} dir" :
+					ciId => "Environments/${customer}/${application}",
+					ciType => 'core.Directory',
+					ciValues => {name => "${application}"},
+					ensure => present,
+					require => Deployit::Features::Ci["${customer} dir"]
+			}
 		}
-	}
-	
-	if ! defined(Deployit::Features::Ci["${remotehost}-ssh-host"]){
-	deployit::features::ci{ "${remotehost}-ssh-host":
- 				 ciId => "Infrastructure/${remotehost}",
-  				 ciType => 'overthere.SshHost',
-  				 ciValues => { os => UNIX, connectionType => SUDO, username => 'deployit', password => 'deployit',
-                 		sudoUsername => 'root', address => "${remotefqdn}", privateKeyFile => "/opt/deployit/keys/jetty_id_rsa" },
-                 		ciEnvironments => "${customer}/${application}/${appstage}/${application}-${appstage}",
-  				 ensure => present,
-  				 require => Deployit::Features::Ci["${customer}/${application}/${appstage}/${application}-${appstage} env"]
-  				 
+		if
+		!defined(Deployit::Features::Ci["${customer}/${application}/${appstage} dir"])
+		{
+			deployit::features::ci {
+				"${customer}/${application}/${appstage} dir" :
+					ciId => "Environments/${customer}/${application}/${appstage}",
+					ciType => 'core.Directory',
+					ciValues => {name => "${appstage}"},
+					ensure => present,
+					require => Deployit::Features::Ci["${customer}/${application} dir"]
+			}
 		}
-	}
-	
+		if
+		!defined(Deployit::Features::Ci["${customer}/${application}/${appstage}/${application}-${appstage} env"])
+		{
+			deployit::features::ci {
+				"${customer}/${application}/${appstage}/${application}-${appstage} env" :
+					ciId =>
+					"Environments/${customer}/${application}/${appstage}/${application}-${appstage}",
+					ciType => 'udm.Environment',
+					ciValues => {name => "${application}-${appstage}"},
+					ensure => present,
+					require => Deployit::Features::Ci["${customer}/${application} dir",
+					"${customer}/${application}/${appstage} dir", "${customer} dir"]
+			}
+		}
+		if !defined(Deployit::Features::Ci["${remotehost}-ssh-host"]) {
+			deployit::features::ci {
+				"${remotehost}-ssh-host" :
+					ciId => "Infrastructure/${remotehost}",
+					ciType => 'overthere.SshHost',
+					ciValues => {os => UNIX, connectionType => SUDO, username => 'deployit',
+					password => 'deployit', sudoUsername => 'root', address =>
+					"${remotefqdn}", privateKeyFile => "/opt/deployit/keys/jetty_id_rsa"},
+					ciEnvironments =>
+					"${customer}/${application}/${appstage}/${application}-${appstage}",
+					ensure => present,
+					require =>
+					Deployit::Features::Ci["${customer}/${application}/${appstage}/${application}-${appstage} env"]
+			}
+		}
 		if !defined(Deployit::Features::Ci["postgresql_database_${db_name}"]) {
 			deployit::features::ci {
 				"postgresql_database_${db_name} " :
@@ -72,7 +77,8 @@ define deployit::types::postgresql_ssh (
 					ciType => 'sql.PostgreSqlClient',
 					ciValues => {postgresqlHome => "$postgresql_home", databaseName =>
 					"${db_name}", username => "${db_username}", password => "${db_password}"},
-					ciEnvironments => "${customer}/${application}/${appstage}/${application}-${appstage}",
+					ciEnvironments =>
+					"${customer}/${application}/${appstage}/${application}-${appstage}",
 					require => Deployit::Features::Ci["${remotehost}-ssh-host"],
 					ensure => present,
 			}
