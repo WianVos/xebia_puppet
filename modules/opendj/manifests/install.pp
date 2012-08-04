18c565aadffe7de6464dec00d79e42bfb1b00371
Wian Vos <wvos@xebia.com>
2012-08-04 14:24:26 +0200

    debugging

--- modules/opendj/manifests/install.pp
+++ modules/opendj/manifests/install.pp
@@ -7,7 +7,8 @@ class opendj::install (
 				xebia_common::source {
 					"${opendj::install_source_url}" :
 						source_url => "${opendj::install_source_url}",
-						target => "${opendj::homedir}"
+						target => "${opendj::homedir}",
+						regdir => "${opendj::markerdir}"
 				}
 				exec {
 					"opendj install" :
